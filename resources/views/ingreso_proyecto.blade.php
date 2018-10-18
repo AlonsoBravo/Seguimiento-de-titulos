240cef8325c3cbdc1294816be4436adb634911bb
Alonso Bravo <42778327+AlonsoBravo@users.noreply.github.com>
2018-10-18 12:52:18 -0200

    Creacion de proyecto
    Se edita el formulario de creacion de proyecto mas css.

--- resources/views/ingreso_proyecto.blade.php
+++ resources/views/ingreso_proyecto.blade.php
@@ -1,75 +1,75 @@
 @include('layouts.header')
 
 @yield('header')
-	<form class="formulario_de_proyecto" action="" method="post">
-			<div class="formulario_proyecto clearfix">
-				<div class="datos_alumnos">
-					<div class="nombre_proyecto">
-							<div class="nombre_de_proyecto">
-								<span>Nombre Proyecto: </span>
-							</div>
-							<div class="titulo_proyecto">
-								<input type="text" name="nombre_proyecto" placeholder="Nombre del proyecto" maxlength="50" required>
-							</div>
-						</div><!-- .nombre_proyecto -->
 
-						<div class="codigo_cursos">
-								<div>
-									<span>Codigo del Curso: </span>
-								</div>
-								<div class="codigo">
-									<select class="opcion">
-										<option value="">--Escoja un codigo--</option>
-										<option value="codigo">xxxx</option>
-									</select>
-								</div>
-							</div><!-- .codigo_curso -->
+<form class="formulario_creacion_proyecto" action="" method="post">
+	<div class="datos_creacion_proyecto">
+		<div class="nombre_proyecto clearfix">
+			<div class="titulo">
+				<span>Nombre proyecto: </span>
+			</div>
+			<div class="ingreso_nombre_proyecto">
+				<input type="text" class="nom_proyecto" placeholder="Ingrese nombre proyecto">
+			</div>
+		</div><!-- .nombre_proyecto -->
+		<div class="codigo_curso clearfix">
+			<div class="titulo">
+				<span>Codigo de curso: </span>
+			</div>
+			<div class="codigo">
+				<select class="opcion">
+					<option value="">"codigo curso"</option>
+				</select>
+			</div>
+		</div><!-- .codigo_curso -->
+			<div class="asignacion_alumno clearfix">
+				<div class="titulo">
+					<span>Asignar alumno: </span>
+				</div>
+				<div class="asignacion">
+					<select class="opcion">
+						<option value="">"Alumno"</option>
+					</select>
+				</div>
+			</div>
+			<div class="crear_proyecto">
+				<button type="submit" class="button" name="button">Crear proyecto</button>
+			</div>
+	</div><!-- .datos_creacion_proyecto -->
+</form><!-- .formulario_creacion_proyecto -->
 
-						<div class="asignar_alumnos">
-								<div>
-									<span>Asignar Alumnos: </span>
-								</div>
-								<div class="alumnos">
-									<select class="opcion">
-										<option value="">-- Alumnos --</option>
-										<option value="nombre">Nombre Apellido</option>
-									</select>
-								</div>
-							</div><!-- .asignar_alumnos -->
-						</div><!-- .datos_alumnos -->
+<form class="editar_eliminar_proyecto" action="" method="post">
+	<div class="grupos_proyectos clearfix">
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Nombre proyecto</th>
+					<th>Codigo curso</th>
+					<th>Integrantes</th>
+				</tr>
+				<tbody>
+					<tr>
+						<td>"Nombre proyecto"</td>
+						<td>"Codigo curso"</td>
+						<td>
+							<p>"Intregante 1"</p>
+							<p>"Intregante 2"</p>
+							<p>"Intregante 3"</p>
+							<p>"Intregante 4"</p>
+						</td>
+						<td>
+							<button class="button" type="submit">Editar</button>
+						</td>
+						<td>
+							<button class="button" type="submit">Eliminar</button>
+						</td>
+					</tr>
+				</tbody>
+			</thead>
+		</table>
+	</div><!-- .grupos_proyectos -->
+</form><!-- .editar_eliminar_proyecto -->
 
-							<div class="grupos clearfix">
-								<div class="titulo">
-									<span>Nombre de proyecto: </span>
-								</div><!-- .titulo -->
-								<div class="nom_proyecto">
-									<span>"Nombre proyecto"</span>
-								</div>
-								<div class="titulo">
-									<span>Codigo de curso: </span>
-								</div><!-- .titulo -->
-								<div class="codigo_curso">
-									<span>"Codigo curso"</span>
-								</div>
-								<div class="nombre_alumnos">
-									<div class="titulo">
-										<span>Integrantes: </span>
-									</div>
-									<div class="grupo_alumnos">
-										<span>"Alumno 1"</span>
-										<span>"Alumno 2"</span>
-										<span>"Alumno 3"</span>
-										<span>"Alumno 4"</span>
-									</div>
-								</div><!-- .nombre_alumnos -->
-								<div class="guardar_proyecto ">
-									<button type="submit" class="guardar button">Crear grupo</button>
-								</div><!-- .guardar_proyecto -->
-							</div><!-- .grupos -->
+@include('layouts.footer')
 
-			</form><!-- .formulario_de_proyecto -->
-		</div><!-- .formulario_proyecto -->
-
-	@include('layouts.footer')
-
-	@yield('footer')
+@yield('footer')
